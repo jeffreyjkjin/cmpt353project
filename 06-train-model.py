2c6c40e1e00b2aaa2915c1ea29b739b8cb200e9b
jeffreyjkjin <jinjeffrey24@gmail.com>
2025-04-03 22:57:15 -0700

    Created 06-train-model.py
    -Added template code and things to do

---
+++ 06-train-model.py
@@ -0,0 +1,11 @@
+import sys
+
+# TODO: implement functions for training each model
+#       save weights for model and as well training/validation set scores
+
+def main(fights, fighters, model, num_runs):
+    # TODO: write a selector to pick which model to train
+    #       train the model num_runs times
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
