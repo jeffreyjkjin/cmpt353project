9525b903102b24f32957ece556100a95bb7c38b8
chl55 <chl55@sfu.ca>
2025-04-10 17:14:50 -0700

    Added support for fight predictions in 07-predictor

--- 05-generate-features.py
+++ 05-generate-features.py
@@ -184,8 +184,8 @@ def computeGlicko(fights, fighters):
 def calculate_totals(df):
     # Sum total for all rounds
     for stat in totals_columns:
-        df[f'sum_red_{stat}'] = df[[col for col in df.columns if f'red_{stat}' in col]].sum(axis=1)
-        df[f'sum_blue_{stat}'] = df[[col for col in df.columns if f'blue_{stat}' in col]].sum(axis=1)
+        df[f'red_sum_{stat}'] = df[[col for col in df.columns if f'red_{stat}' in col]].sum(axis=1)
+        df[f'blue_sum_{stat}'] = df[[col for col in df.columns if f'blue_{stat}' in col]].sum(axis=1)
 
     return df
 
@@ -193,22 +193,22 @@ def calculate_percentages(df):
     # Calculates Percentages for stats with landed & attempts
     for stat in percents_columns:
         # For red stats
-        df[f'red_{stat}_pct'] = df[f'sum_red_{stat}_lnd'] / df[f'sum_red_{stat}_att']
+        df[f'red_{stat}_pct'] = df[f'red_sum_{stat}_lnd'] / df[f'red_sum_{stat}_att']
         # Set to 0 if division results in NaN or if sum_red_{stat}_att is 0
-        df[f'red_{stat}_pct'] = df[f'red_{stat}_pct'].where(df[f'sum_red_{stat}_att'] != 0, 0)
+        df[f'red_{stat}_pct'] = df[f'red_{stat}_pct'].where(df[f'red_sum_{stat}_att'] != 0, 0)
 
         # For blue stats
-        df[f'blue_{stat}_pct'] = df[f'sum_blue_{stat}_lnd'] / df[f'sum_blue_{stat}_att']
+        df[f'blue_{stat}_pct'] = df[f'blue_sum_{stat}_lnd'] / df[f'blue_sum_{stat}_att']
         # Set to 0 if division results in NaN or if avg_blue_{stat}_att is 0
-        df[f'blue_{stat}_pct'] = df[f'blue_{stat}_pct'].where(df[f'sum_blue_{stat}_att'] != 0, 0)
+        df[f'blue_{stat}_pct'] = df[f'blue_{stat}_pct'].where(df[f'blue_sum_{stat}_att'] != 0, 0)
     
     return df
 
 def calculate_averages(df):
     # calculate per round stats
     for stat in averages_columns:
-        df[f'avg_red_{stat}'] = df[f'sum_red_{stat}'] / df['round']
-        df[f'avg_blue_{stat}'] = df[f'sum_blue_{stat}'] / df['round']
+        df[f'red_avg_{stat}'] = df[f'red_sum_{stat}'] / df['round']
+        df[f'blue_avg_{stat}'] = df[f'blue_sum_{stat}'] / df['round']
     return df
 
 def main(in_dir1, in_dir2, out_dir1, out_dir2):
---
+++ 07-predictor.py
@@ -0,0 +1,95 @@
+import sys
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import joblib
+
+def stance_to_num(stance):
+    """ Convert stance string to numeric """
+    stance_mapping = {'Orthodox': 1, 'Southpaw': 2, 'Switch': 3}
+    return stance_mapping.get(stance, 0)
+
+# Returns the probability that fighter1 (red) wins
+def predict_winner(fighter1, fighter2, model, column_list, scaler):
+    fighter1_name = fighter1['name']
+    fighter2_name = fighter2['name']
+    
+    fighter1 = fighter1.to_frame().T
+    fighter2 = fighter2.to_frame().T 
+    
+    # Drop unnecessary columns
+    fighter1 = fighter1.drop(columns=['name', 'volatility'])
+    fighter2 = fighter2.drop(columns=['name', 'volatility'])
+    
+    # Add prefix to columns
+    fighter1 = fighter1.add_prefix('red_')
+    fighter2 = fighter2.add_prefix('blue_')
+    
+    # Convert stance to numeric
+    fighter1['red_stance'] = fighter1['red_stance'].apply(stance_to_num)
+    fighter2['blue_stance'] = fighter2['blue_stance'].apply(stance_to_num)
+    
+    fighter1.reset_index(drop=True, inplace=True)
+    fighter2.reset_index(drop=True, inplace=True)
+    
+    # Concatenate the two fighters into a single DataFrame
+    fighter_data = pd.concat([fighter1, fighter2], axis=1)
+    
+    fighter_data = fighter_data[column_list]
+    
+    print(fighter_data)
+    
+    # Scale the data
+    fighter_data = scaler.transform(fighter_data)
+    # Convert back to DataFrame to keep the column names
+    fighter_data = pd.DataFrame(fighter_data, columns=column_list)
+    
+    prediction = model.predict(fighter_data)
+    
+    print("Winner: ", fighter1_name if prediction[0] >=0.5 else fighter2_name)
+    print(f'{fighter1_name} win probability: {prediction[0]:.2f}')
+    print(f'{fighter2_name} win probability: {1 - prediction[0]:.2f}')
+
+
+
+def main(model_path, fighter_data, input_path, output_path):
+    # Load the model
+    model, column_list, scaler = joblib.load(model_path)
+
+    # Fighter list
+    names = []
+
+    # Open the file and read line by line
+    with open(input_path, 'r') as file:
+        for line in file:
+            names.append(line.strip())
+    print(names)
+            
+    # Check if the number of fighters is a power of 2 and more than 2 players
+    if len(names) < 2 or (len(names) & (len(names) - 1)) != 0:
+        print("Error: The number of fighters must be 2 or greater or a power of 2.")
+        return
+
+    # Read the full fighter data
+    df = pd.read_csv(fighter_data)
+
+    # Collect recognized and unrecognized names
+    recognized_names = [name for name in names if name in df['name'].values]
+    unrecognized_names = [name for name in names if name not in df['name'].values]
+
+    # Filter rows based on recognized names
+    fighters = df[[name in recognized_names for name in df['name']]]
+    fighters = df.set_index('name').loc[recognized_names].reset_index()
+    print(fighters)
+
+    # Print unrecognized names
+    if unrecognized_names:
+        print(f"Cannot find the following fighters: {', '.join(unrecognized_names)}")
+        return
+        
+    predict_winner(fighters.iloc[0], fighters.iloc[1], model, column_list, scaler)
+        
+if __name__ == '__main__':
+    """
+    Usage: python 07-predictor.py <model_path> <fighter_data> <input_path> <output_path>
+    """
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
