83dc5f4b65f142e6a3601433f30af6e62bf0b9bf
jeffreyjkjin <jinjeffrey24@gmail.com>
2025-04-12 16:25:38 -0700

    Created 09-analyze-predictor.py
    -Implemented code to run normality test on model accuracies, create histogram of accuracies, and run anova and posthoc tukey on model accuracies
    -Added sample plot for 09 histogram
    -Added statsmodels library to requirements.txt

---
+++ 09-analyze-predictor.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn
+import sys
+
+from scipy import stats
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
+
+seaborn.set_theme()
+
+def main(predictor_results):
+    df = pd.read_csv(predictor_results)
+
+    # separate models
+    svm = df[df['name'] == 'svm']['accuracy']
+    tree = df[df['name'] == 'tree']['accuracy']
+    forest = df[df['name'] == 'forest']['accuracy']
+
+    # create histogram of model accuracies
+    plt.figure()
+    plt.hist(svm, color='r', alpha=0.5)
+    plt.hist(tree, color='g', alpha=0.5)
+    plt.hist(forest, color='b', alpha=0.5)
+    plt.legend(['SVM', 'Tree', 'Forest'])
+    plt.xlabel('Accuracy')
+    plt.ylabel('Frequency')
+    plt.title('Frequency of Model Accuracies')
+    plt.savefig('09-model-accuracy.png')
+
+    # normal test on classifier accuracy
+    print(f'Normal test on SVM Accuracy: {stats.normaltest(svm).pvalue}')
+    print(f'Normal test on Tree Accuracy: {stats.normaltest(tree).pvalue}')
+    print(f'Normal test on Forest Accuracy: {stats.normaltest(forest).pvalue}')
+    print('')
+
+    anova = stats.f_oneway(svm, tree, forest)
+    print(f'ANOVA p-value: {anova.pvalue}\n')
+
+    # tukey posthoc analysis
+    df_melt = pd.melt(pd.DataFrame(
+        {'svm': svm.to_list(), 
+         'tree': tree.to_list(), 
+         'forest': forest.to_list()}
+    ))
+    posthoc = pairwise_tukeyhsd(df_melt['value'], df_melt['variable'], alpha=0.05)
+    print(posthoc)
+
+if __name__ == '__main__':
+    main(sys.argv[1])
